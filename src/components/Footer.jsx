c78ac562502cf4edc895257c10eebdb4c319c914
sunday <iamdyclef@gmail.com>
2025-09-07 23:40:43 +0100

    feat: working on fixing website responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,22 +3,22 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-black px-6 py-8 grid md:flex">
+    <div className="bg-black px-2 md:px-6 py-12 md:py-8 flex flex-col md:flex-row justify-evenly  ">
       
-      <div className="flex-[2] row-span-3">
+      <div className="md:flex-1 ">
         <h1 className="text-green-300 font-bold text-3xl">
           DATALY. </h1>
-          <p className="text-gray-300 ">
+          <p className="text-gray-300 pr-6">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit
             ullam iste repellat consequatur libero reiciendis, blanditiis
             accusantium.
           </p>
        
       </div>
-      <div className="flex flex-1">
+      <div className="flex md:flex-col md:flex-[2] gap-5 md:gap-40 justify-between">
       <div className="flex-1">
         <h2 className="text-gray-400 font-medium">Solutions</h2>
-        <ul className="text-gray-300 text-sm leading-8">
+        <ul className="text-goffnmkray-300 text-sm leading-8">
           <li>Analytics</li>
           <li>Marketting</li>
           <li>Commerce</li>
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -3,16 +3,16 @@ import Button from './Button';
 
 const Newsletter = () => {
   return  (
-    <div className='bg-black py-14 px-3 flex justify-between' >
-      <div className='flex flex-col justify-center '>
-        <h1 className='text-white font-bold text-4xl '>Want tips & tricks to <span className='text-green-400'>optimize</span> your flow?</h1>
-        <p className='text-white'>Sign up to our newsletter and stay up to date..</p>
+    <div className='bg-black py-16 px-3 md:flex md:justify-between' >
+      <div className='flex flex-col justify-center my-5 md:my-0 '>
+        <h1 className='text-white font-bold text-2xl md:text-4xl '>Want tips & tricks to <span className='text-green-400'>optimize</span> your flow?</h1>
+        <p className='text-white my-2 md:my-0'>Sign up to our newsletter and stay up to date..</p>
       </div>
-      <div className='ml-20  flex-1'>
-        <div className=' flex gap-5 '>
+      <div className=' md:ml-20  md:flex-1'>
+        <div className=' md:flex md:gap-5 '>
           <input type="email" name="" id="" placeholder='Enter Email'
-          className='bg-white py-3 my-6 rounded-md pl-3'/>
-          <Button content={"Notify Me"} type= {"button"} className={"bg-green-300 text-black mx-0 w-[200px] py-3 rounded-md font-medium my-6 hover:bg-green-600 hover:text-white cursor-pointer"} />
+          className='bg-white py-3 my-1 md:my-6 rounded-md pl-3 w-[205px] md:w-[200px]'/>
+          <Button content={"Notify Me"} type= {"button"} className={"bg-green-300 text-black mx-0 w-[200px] py-3 rounded-md font-medium my-2 md:my-6 hover:bg-green-600 hover:text-white cursor-pointer"} />
 
         </div>
         <div className='mr-15'><p className='text-white'>We care bout the protection of your data. Read our <span className='text-green-400'>Privacy Policy</span>.</p></div>
